01cb2e87265e7be12cb919b6dd0eac9ad97e7102
Cai <trieungochai.dev@gmail.com>
2021-07-29 14:30:51 +0700

    Introducing React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <p>Page 1</p>;
+};
+
+const PageTwo = () => {
+  return <p>Page 2</p>;
+};
 
 const App = () => {
-  return <p>See you SpaceCowboy!!!</p>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
