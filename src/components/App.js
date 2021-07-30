1f10e0f56e7b3081c7ef25596e6cc44a4210199c
Cai <trieungochai.dev@gmail.com>
2021-07-30 15:18:36 +0700

    Connecting the Header

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,12 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
+      <Header />
       <BrowserRouter>
         <div>
           <Route path="/" exact component={StreamList} />
