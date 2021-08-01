f9708584536e423e9d4776b4b39cf75bc908512e
Cai <trieungochai.dev@gmail.com>
2021-08-01 12:58:16 +0700

    Building the Auth Reducer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import reducers from "./reducers";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
 const store = createStore(reducers);
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import authReducer from "./authReducer";
 
 export default combineReducers({
-  replaceMe: () => "See you Space Cowboy!!!",
+  auth: authReducer,
 });
