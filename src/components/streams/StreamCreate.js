cf5a413a5a48987bd78e18820279f65824ae578d
Cai <trieungochai.dev@gmail.com>
2021-08-02 23:31:15 +0700

    Validation of Form Inputs and Displaying Validation Messages

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { Field, formValues, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
-    console.log({ input });
+  renderInput({ input, label, meta }) {
+    console.log({ meta });
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
 
   onSubmit(formValues) {
-    console.log(formValues);
+    console.log({ formValues });
   }
 
   render() {
     console.log(this.props);
     return (
-      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
@@ -32,4 +36,18 @@ class StreamCreate extends React.Component {
   }
 }
 
-export default reduxForm({ form: "streamCreate" })(StreamCreate);
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title!!!";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description!!!";
+  }
+
+  return errors;
+};
+
+export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
