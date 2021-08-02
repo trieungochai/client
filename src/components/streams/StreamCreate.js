fe93e7187be10441d370a2288b5ce8fc098223b6
Cai <trieungochai.dev@gmail.com>
2021-08-02 23:06:08 +0700

    Handling Form Submission

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -12,16 +12,21 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     console.log(this.props);
     return (
-      <form className="ui form">
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
           component={this.renderInput}
           label="Enter Description"
         />
+        <button className="ui primary button">Submit</button>
       </form>
     );
   }
