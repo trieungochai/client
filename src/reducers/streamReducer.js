5016616d9f1a855900654def48811afbd9d64124
Cai <trieungochai.dev@gmail.com>
2021-08-08 15:31:53 +0700

    Merging Lists of Records

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,15 +1,18 @@
 import _ from "lodash";
+
 import {
   CREATE_STREAM,
   DELETE_STREAM,
   EDIT_STREAM,
   FETCH_STREAM,
   FETCH_STREAMS,
-} from "./types";
+} from "../actions/types";
 
 // Object-based approach
 const streamReducer = (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
