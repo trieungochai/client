16a02b58bb3151b445154288bdd28ca56cca367f
Cai <trieungochai.dev@gmail.com>
2021-08-09 22:22:52 +0700

    Creating a Browser History Object

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 // Different Router Types
 // https://www.udemy.com/course/react-redux/learn/lecture/12700551#overview
+import history from "../history";
 
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
@@ -13,7 +14,7 @@ import Header from "./Header";
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -22,7 +23,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
