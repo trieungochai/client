6018822a0d3194a501df180d173392b345e33f79
Cai <trieungochai.dev@gmail.com>
2021-08-11 21:55:45 +0700

    OnDismiss from the Parent

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import history from "../../history";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
@@ -16,6 +18,7 @@ const StreamDelete = () => {
         title="Delete Stream"
         content="Are you sure you want to delete this stream?"
         actions={actions}
+        onDismiss={() => history.push("/")}
       />
     </div>
   );
