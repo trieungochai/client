c159f878f32fd71613bac6232512c8863971f552
Cai <trieungochai.dev@gmail.com>
2021-08-13 23:08:05 +0700

    Optional Player Building

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -13,15 +13,27 @@ class StreamShow extends React.Component {
 
   componentDidMount() {
     const { id } = this.props.match.params;
-    
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: "flv",
       url: `http://localhost:8000/live/${id}.flv`,
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
-  }
+  };
 
   render() {
     console.log(this.props);
@@ -42,7 +54,6 @@ class StreamShow extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  console.log({ state, ownProps });
   return {
     stream: state.streams[ownProps.match.params.id],
   };
